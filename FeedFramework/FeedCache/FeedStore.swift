329c707d420c4852fc37d3aa0ea3db61b20b409d
Dmytro Rebenko <dima.rebenko@gmail.com>
2020-04-02 21:14:32 +0300

    Added informative comments

--- FeedFramework/FeedCache/FeedStore.swift
+++ FeedFramework/FeedCache/FeedStore.swift
@@ -10,8 +10,13 @@ public protocol FeedStore {
     typealias DeleteCacheCompletion = (Error?) -> ()
     typealias InsertionCompletion = (Error?) -> ()
     typealias RetrivalCompletion = (RetrieveCachedFeedResult) -> ()
-    
+    // Completion can be called at any Thread
+    // Client are responsible for dispatch it on correct thread
     func deleteCachedFeed(completion: @escaping (Error?) -> ())
+    // Completion can be called at any Thread
+    // Client are responsible for dispatch it on correct thread
     func insert(_ feedItems: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    // Completion can be called at any Thread
+    // Client are responsible for dispatch it on correct thread
     func retrieve(completion: @escaping RetrivalCompletion)
 }
