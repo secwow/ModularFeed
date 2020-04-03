e75ebdb22dc3f1ea894f75f1468bef4efa41b597
Dmytro Rebenko <dima.rebenko@gmail.com>
2020-04-03 22:29:44 +0300

    Added new target to separate potentially slow integration tests from unit tests.

---
+++ FeedFrameworkIntegrationTests/FeedFrameworkIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedFrameworkIntegrationTests.swift
+//  FeedFrameworkIntegrationTests
+//
+//  Created by AndAdmin on 03.04.2020.
+//  Copyright © 2020 AndAdmin. All rights reserved.
+//
+
+import XCTest
+
+class FeedFrameworkIntegrationTests: XCTestCase {
+
+    func 
+}
