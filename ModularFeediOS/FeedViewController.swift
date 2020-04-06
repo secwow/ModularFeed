44cf790cfb8a4c29f3e8f5d17707da9b5c31bde0
Dmytro Rebenko <dima.rebenko@gmail.com>
2020-04-06 09:47:19 +0300

    Added a testable files to target.

---
+++ ModularFeediOS/FeedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.swift
+//  ModularFeediOS
+//
+//  Created by AndAdmin on 06.04.2020.
+//  Copyright © 2020 AndAdmin. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+}
